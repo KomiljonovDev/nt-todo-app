9aa68f7861ffbfed54fcb61069c154b47c04a5a5
Komiljonov O <uzokdeveloper@gmail.com>
2024-12-05 19:39:08 +0500

    MINOR: Implemented the php dotenv package

--- index.php
+++ index.php
@@ -1,59 +1,53 @@
 <?php
 
-require 'vendor/autoload.php';
+require 'bootstrap.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+require 'src/Todo.php';
+require 'helpers.php';
+require 'src/Router.php';
 
-var_dump($_ENV['DB_HOST']);
-var_dump($_ENV['DB_PASSWORD']);
+$router = new Router();
+$todo = new Todo();
 
-//require 'src/Todo.php';
-//require 'helpers.php';
-//require 'src/Router.php';
-//
-//$router = new Router();
-//$todo = new Todo();
-//
-//$router->put('/todos/{id}/update', function ($todoId) use($todo){
-//    $todo->update(
-//        $todoId,
-//        $_POST['title'],
-//        $_POST['status'],
-//        $_POST['due_date']
-//    );
-//    redirect('/todos');
-//});
-//
-//
-//$router->get('/todos/{id}/update', function ($todoId) use($todo){
-//    echo 'Edit the task: ' . $todoId;
-//    $getTodo = $todo->getTodo($todoId);
-//    view('edit', [
-//        'todo'=>$getTodo
-//    ]);
-//});
-//
-//$router->get('/', function () {
-//    view('home');
-//});
-//
-//$router->get('/todos', function () use ($todo) {
-//    $todos = $todo->getAllTodos();
-//    view('todos', ['todos' => $todos]);
-//});
-//
-//$router->post('/todos', function () use ($todo) {
-//    if (!empty($_POST['title']) && !empty($_POST['due_date'])) {
-//        $todo->store($_POST['title'], $_POST['due_date']);
-//        redirect('/todos');
-//    }
-//});
-//
-//$router->get('/todos/{id}/delete', function ($todoId) use($todo){
-//    $todo->destroy($todoId);
-//    redirect('/todos');
-//});
+$router->put('/todos/{id}/update', function ($todoId) use($todo){
+    $todo->update(
+        $todoId,
+        $_POST['title'],
+        $_POST['status'],
+        $_POST['due_date']
+    );
+    redirect('/todos');
+});
+
+
+$router->get('/todos/{id}/update', function ($todoId) use($todo){
+    echo 'Edit the task: ' . $todoId;
+    $getTodo = $todo->getTodo($todoId);
+    view('edit', [
+        'todo'=>$getTodo
+    ]);
+});
+
+$router->get('/', function () {
+    view('home');
+});
+
+$router->get('/todos', function () use ($todo) {
+    $todos = $todo->getAllTodos();
+    view('todos', ['todos' => $todos]);
+});
+
+$router->post('/todos', function () use ($todo) {
+    if (!empty($_POST['title']) && !empty($_POST['due_date'])) {
+        $todo->store($_POST['title'], $_POST['due_date']);
+        redirect('/todos');
+    }
+});
+
+$router->get('/todos/{id}/delete', function ($todoId) use($todo){
+    $todo->destroy($todoId);
+    redirect('/todos');
+});
 
 
 
